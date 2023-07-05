454f2d51ceb0acd40eee1834bb3eb3cca2dea99e
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-07-05 20:24:50 +0900

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!-updated-github-action-applied-pushWithPRFromFeature-test2';
+    return 'change workflow to use echo';
   }
 }
